2ba3b9dc80687cf07a82f3b356a1ee1ea9aebc51
Adam Jedlička <adajedlicka@gmail.com>
2019-07-28 18:15:30 +0200

    Math.ceil

--- src/vm/lib/math/math.c
+++ src/vm/lib/math/math.c
@@ -11,9 +11,18 @@ int8_t Math__floor(bluVM* vm, int8_t argCount, bluValue* args) {
 	return 1;
 }
 
+int8_t Math__ceil(bluVM* vm, int8_t argCount, bluValue* args) {
+	double number = AS_NUMBER(args[1]);
+
+	args[0] = NUMBER_VAL((int)number + 1);
+
+	return 1;
+}
+
 void bluInitMath(bluVM* vm) {
 	bluInterpret(vm, mathSource, "__MATH__");
 
 	bluObj* mathClass = bluGetGlobal(vm, "Math");
 	bluDefineStaticMethod(vm, mathClass, "floor", Math__floor, 1);
+	bluDefineStaticMethod(vm, mathClass, "ceil", Math__ceil, 1);
 }
