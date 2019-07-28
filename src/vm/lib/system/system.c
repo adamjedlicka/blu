e5ea8e55816328ecb4dc1d3bf62857a5405ad5bc
Adam Jedlička <adajedlicka@gmail.com>
2019-07-28 17:19:12 +0200

    System.readline

--- src/vm/lib/system/system.c
+++ src/vm/lib/system/system.c
@@ -1,4 +1,6 @@
 #include "system.h"
+#include "vm/memory.h"
+#include "vm/object.h"
 #include "vm/value.h"
 
 #include "system.blu.inc"
@@ -16,10 +18,25 @@ int8_t System__println(bluVM* vm, int8_t argCount, bluValue* args) {
 	return 1;
 }
 
+int8_t System__readline(bluVM* vm, int8_t argCount, bluValue* args) {
+	char* line = bluAllocate(vm, sizeof(char) * 1024);
+
+	if (!fgets(line, 1024, stdin)) {
+		printf("\n");
+	}
+
+	bluObjString* string = bluTakeString(vm, line, strlen(line) - 1);
+
+	args[0] = OBJ_VAL(string);
+
+	return 1;
+}
+
 void bluInitSystem(bluVM* vm) {
 	bluInterpret(vm, systemSource, "__SYSTEM__");
 
 	bluObj* systemClass = bluGetGlobal(vm, "System");
 	bluDefineStaticMethod(vm, systemClass, "print", System__print, 1);
 	bluDefineStaticMethod(vm, systemClass, "println", System__println, 1);
+	bluDefineStaticMethod(vm, systemClass, "readline", System__readline, 0);
 }
