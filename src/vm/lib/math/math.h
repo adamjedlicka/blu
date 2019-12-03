7795c4c4909d675ab933afac147219e45ad39b1d
Adam Jedlička <adajedlicka@gmail.com>
2019-12-03 11:55:44 +0100

    Removed Math module

--- src/vm/lib/math/math.h
+++
@@ -1,8 +0,0 @@
-#ifndef blu_math_h
-#define blu_math_h
-
-#include "include/blu.h"
-
-void bluInitMath(bluVM* vm);
-
-#endif
--- src/vm/lib/std.c
+++ src/vm/lib/std.c
@@ -2,13 +2,11 @@
 
 #include "vm/lib/core/core.h"
 #include "vm/lib/file/file.h"
-#include "vm/lib/math/math.h"
 #include "vm/lib/system/system.h"
 
 void bluInitStd(bluVM* vm) {
 	bluInitCore(vm);
 
 	bluRegisterModule(vm, "system", bluInitSystem);
-	bluRegisterModule(vm, "math", bluInitMath);
 	bluRegisterModule(vm, "file", bluInitFile);
 }
